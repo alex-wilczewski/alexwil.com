bf5cdd6ab510f1e90a26a7a965639cc3c1ecdb1b
Alex Wilczewski <alex@Alexs-MacBook-Pro-4.local>
2026-01-25 22:11:23 -0500

    Updates

---
+++ _next/static/chunks/app/layout-35b5bb0b0c87bc4c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3836:()=>{},7394:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1356,23)),Promise.resolve().then(n.t.bind(n,3836,23))}},e=>{e.O(0,[475,356,441,255,358],()=>e(e.s=7394)),_N_E=e.O()}]);
